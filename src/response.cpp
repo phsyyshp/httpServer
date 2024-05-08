c2b50b67f8d26118029c3623defe50596563133f
“physsyhp” <27218386+phsyyshp@users.noreply.github.com>
2024-05-07 23:07:14 -0400

    pass stage

--- src/response.cpp
+++ src/response.cpp
@@ -13,9 +13,9 @@ std::string Response::respond(const Request &request) const {
   std::string space = "\r\n";
   startLine = "HTTP/1.1 200 OK\r\n";
   std::string contentType = "Content-Type: text/plain\r\n";
-  auto body = path + "\r\n";
+  auto body = path;
   std::string contentLength =
-      "Content-Length:" + std::to_string(path.length()) + space;
+      "Content-Length:" + std::to_string(body.length()) + space;
 
   out = startLine + contentType + contentLength + "\r\n\r\n" + body;
   //   if (request.parseRequestLine()["path"] == "/") {
