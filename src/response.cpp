47072df48641d20db88532f756ca70a02d8f48ea
“physsyhp” <27218386+phsyyshp@users.noreply.github.com>
2024-05-07 23:19:35 -0400

    test

--- src/response.cpp
+++ src/response.cpp
@@ -6,22 +6,23 @@ std::string Response::respond(const Request &request) const {
   std::unordered_map<std::string, std::string> responseMap;
   std::string path = request.parseRequestLine()["path"];
   std::string textToEcho;
+
+  startLine = "HTTP/1.1 200 OK\r\n";
   auto idx = path.find("/echo/", 0);
+  std::string space = "\r\n";
   if (idx != std::string::npos) {
     path.erase(path.begin() + idx, path.begin() + 6);
-  }
-  std::string space = "\r\n";
-  startLine = "HTTP/1.1 200 OK\r\n";
-  std::string contentType = "Content-Type: text/plain\r\n";
-  auto body = path;
-  std::string contentLength =
-      "Content-Length:" + std::to_string(body.length()) + space;
+    std::string contentType = "Content-Type: text/plain\r\n";
+    auto body = path;
+    std::string contentLength =
+        "Content-Length:" + std::to_string(body.length()) + space;
 
-  out = startLine + contentType + contentLength + "\r\n" + body;
-  //   if (request.parseRequestLine()["path"] == "/") {
-  //   } else {
-  //     startLine = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
-  //   }
-  //   retrun
+    out = startLine + contentType + contentLength + "\r\n" + body;
+  } else if (request.parseRequestLine()["path"] == "/") {
+    out = startLine + space;
+
+  } else {
+    out = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+  }
   return out;
 }
