e67ec9bc6d1a49c66672fc58260022cb4d1dfeec
“physsyhp” <27218386+phsyyshp@users.noreply.github.com>
2024-05-07 23:12:44 -0400

    pass stage

--- src/server.cpp
+++ src/server.cpp
@@ -74,7 +74,7 @@ int main(int argc, char **argv) {
   responseBuffer = response.respond(request);
   std::cout << responseBuffer;
 
-  send(socket_fd, responseBuffer.data(), responseBuffer.size() - 1, 0);
+  send(socket_fd, responseBuffer.data(), responseBuffer.size(), 0);
 
   close(server_fd);
 
