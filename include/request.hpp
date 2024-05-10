b63b3ce7d4f647f23224b95e22ce40fdf4748b85
“physsyhp” <27218386+phsyyshp@users.noreply.github.com>
2024-05-09 21:18:32 -0400

    tests

--- include/request.hpp
+++ include/request.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <algorithm>
 #include <array>
 #include <fstream>
 #include <iostream>
@@ -14,16 +15,15 @@ struct RequestLine {
 class Request {
 public:
   Request();
-  void preProcess(std::array<char, 1024> &buffer);
   void parse(std::array<char, 1024> &buffer);
-  bool isWhiteSpace(char) const;
   RequestLine getRequestLine() const;
   std::unordered_map<std::string, std::string> getHeaderHash() const;
   std::array<char, 1024> getBody() const;
 
 private:
+  bool isWhiteSpace(char) const;
+  void preProcess(std::array<char, 1024> &buffer);
   RequestLine requestLine;
-
   std::unordered_map<std::string, std::string> headersHash;
   std::array<char, 1024> body;
 };
