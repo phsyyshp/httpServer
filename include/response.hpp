c1f9e1b6364d99f31598c57367bf841d27d14dc2
“physsyhp” <27218386+phsyyshp@users.noreply.github.com>
2024-05-10 00:23:58 -0400

    test

--- include/response.hpp
+++ include/response.hpp
@@ -6,10 +6,10 @@ public:
   Response() {}
   std::string respond(const Request &request,
                       const std::string &dir = "") const;
-  std::string get(const Request &request,
-                      const std::string &dir = "") const;
-  std::string post(const Request &request,
-                      const std::string &dir = "") const;
-                       
-                       
+  std::string get(const Request &request, const std::string &dir = "") const;
+  std::string post(const Request &request, const std::string &dir = "") const;
+
+  std::string statusLine(const Request &request, int statusCode) const;
+  std::string contentHeaders(const std::string contentType,
+                             int contentLength) const;
 };
--- src/response.cpp
+++ src/response.cpp
@@ -1,4 +1,5 @@
 #include "response.hpp"
+#include <string>
 std::string Response::respond(const Request &request,
                               const std::string &dir) const {
 
@@ -52,67 +53,63 @@ std::string Response::post(const Request &request,
 std::string Response::get(const Request &request,
                           const std::string &dir) const {
 
-  std::string out;
   std::string body;
-  std::string startLine;
-  std::string contentLength;
-  std::string contentType;
-  std::string line;
+  std::string contentHeader;
   RequestLine requestLine = request.getRequestLine();
   std::string requestTarget = requestLine.requestTarget;
-  std::string textToEcho;
-  std::string space = "\r\n";
-  startLine = "HTTP/1.1 200 OK\r\n";
-  auto idx = requestTarget.find("/echo/", 0);
-  auto idx2 = requestLine.requestTarget.find("/files/", 0);
-  std::string fileName;
 
-  // std::cout << "halala";
   if (requestLine.requestTarget == "/") {
-    contentType = "Content-Type: text/plain\r\n";
-    contentLength = "Content-Length:" + std::to_string(4) + space;
-    return startLine + contentType + contentLength + "Connection: close\r\n" +
-           "\r\n" + "pass";
+    return statusLine(request, 200) + contentHeaders("text/plain", 0);
   }
 
-  if (idx != std::string::npos) {
-    requestTarget.erase(requestTarget.begin() + idx, requestTarget.begin() + 6);
-    body = requestTarget;
-  } else if (requestLine.requestTarget == "/user-agent") {
-    body = request.getHeaderHash()["User-Agent"];
-  } else if (idx2 != std::string::npos) {
-    requestTarget.erase(requestTarget.begin() + idx2,
-                        requestTarget.begin() + 7);
-    fileName = requestTarget;
+  if (requestTarget.find("/echo/", 0) != std::string::npos) {
+    requestTarget.erase(requestTarget.begin(), requestTarget.begin() + 6);
+    return statusLine(request, 200) +
+           contentHeaders("text/plain", requestTarget.length()) + requestTarget;
+  }
+  if (requestLine.requestTarget == "/user-agent") {
+    return statusLine(request, 200) +
+           contentHeaders("text/plain",
+                          request.getHeaderHash()["User-Agent"].length()) +
+           "\r\n" + request.getHeaderHash()["User-Agent"];
+  }
+  if (requestLine.requestTarget.find("/files/", 0) != std::string::npos) {
+    requestTarget.erase(requestTarget.begin(), requestTarget.begin() + 7);
     std::ifstream file;
-    file.open(dir + "/" + fileName);
-    std::cout << fileName;
+    file.open(dir + "/" + requestTarget);
     if (!file) {
-      startLine = "HTTP/1.1 404 NOT FOUND\r\n";
-      contentType = "Content-Type: text/plain\r\n";
-      contentLength = "Content-Length:" + std::to_string(3) + space;
-      return startLine + contentType + contentLength + "Connection: close\r\n" +
-             "\r\n" + "err";
+      return statusLine(request, 404) + contentHeaders("text/plain", 0);
     }
-
-    startLine = "HTTP/1.1 200 OK\r\n";
-    contentType = "Content-Type: application/octet-stream\r\n";
+    std::string line;
     while (getline(file, line)) {
       body += line;
     }
-    contentLength = "Content-Length:" + std::to_string(body.length()) + space;
-    return startLine + contentType + contentLength + "\r\n" + body;
-
+    return statusLine(request, 200) +
+           contentHeaders("application/octet-stream", body.length()) + "\r\n" +
+           body;
   } else {
-    startLine = "HTTP/1.1 404 NOT FOUND\r\n";
-    contentType = "Content-Type: text/plain\r\n";
-    contentLength = "Content-Length:" + std::to_string(3) + space;
-    return startLine + contentType + contentLength + "Connection: close\r\n" +
-           "\r\n" + "err";
+    return statusLine(request, 200) + contentHeaders("text/plain", 0);
   }
+}
 
-  contentType = "Content-Type: text/plain\r\n";
-  contentLength = "Content-Length:" + std::to_string(body.length()) + space;
+std::string Response::statusLine(const Request &request, int statusCode) const {
+  std::string reasonPhrase;
+  switch (statusCode) {
+  case 200:
+    reasonPhrase = "OK";
+    break;
+  case 400:
+    reasonPhrase = "NOT FOUND";
+    break;
+  default:
+    break;
+  }
+  return request.getRequestLine().version + " " + std::to_string(statusCode) +
+         " " + reasonPhrase + "\r\n";
+}
+std::string Response::contentHeaders(const std::string contentType,
+                                     int contentLength) const {
 
-  return startLine + contentType + contentLength + "\r\n" + body;
+  return "Content-Type: " + contentType + "\r\n" +
+         "Content-Length: " + std::to_string(contentLength) + "\r\n";
 }
