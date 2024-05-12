049716cf8f0395875fcbb39890b718b5f2a5bcf4
physsyhp <27218386+phsyyshp@users.noreply.github.com>
2024-05-11 22:47:49 -0400

    test[A

--- src/response.cpp
+++ src/response.cpp
@@ -70,6 +70,7 @@ std::string Response::get(const Request &request,
     //   std::cout << key << value << '\n';
     // }
     if (request.getHeaderHash()["Accept-Encoding"] == "gzip") {
+      std::cout << "calisio\n";
       cmd.encoding = "gzip";
     }
     return statusLine(request, 200) + contentHeaders(cmd) + "\r\n" +
